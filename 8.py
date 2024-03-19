6fe305649d6c0ccb1d5f9f8afb104847ecb7daf1
mrgaritka <m.kabanova@g.nsu.ru>
2024-03-19 14:27:36 +0300

    8

--- 8.py
+++ 8.py
@@ -1,16 +1,14 @@
-import datetime as d
-
 name = input('Здравствуйте! Как Вас зовут? ')
 print('Очень приятно, ', name, '!', ' Меня зовут Марк.', sep='')
 age = int(input('Сколько Вам лет? '))
-if d.datetime.now().year - age > 1944:
-    print('Да, ', name, ', я старше Вас на ', (d.datetime.now().year - age)-1944, ' лет.',
+if 2024 - age > 1944:
+    print('Да, ', name, ', я старше Вас на ', (2024 - age)-1944, ' лет.',
           sep='')
-elif d.datetime.now().year - age < 1944:
-    print('Да, ', name, ', Вы старше меня на ', 1944-(d.datetime.now().year - age), ' лет.',
+elif 2024 - age < 1944:
+    print('Да, ', name, ', Вы старше меня на ', 1944-(2024 - age), ' лет.',
           sep='')
 else:
-    print('Да, ', name, ' мне тоже ', d.datetime.now().year-1944, sep='')
+    print('Да, ', name, ' мне тоже ', 2024 - 1944, sep='')
 ans = input('Вам нравится программировать? ')
 if ans == 'Да' or ans == 'да':
     print('Превосходно! Уверен, у Вас получится написать множество полезных и хороших программ.')
